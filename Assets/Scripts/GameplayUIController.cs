ac51d78bc0a0fad38bf6b6f290ae284d3032a710
suhrusai <46128878+suhrusai@users.noreply.github.com>
2021-08-16 17:50:30 +0530

    Added Play Pause Toggle Button
    Added Play Pause toggle button .
    When the game is on pause it displays resume button and vice versa

--- Assets/Scripts/GameplayUIController.cs
+++ Assets/Scripts/GameplayUIController.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameplayUIController : MonoBehaviour
 {
     bool GamePause = false;
+    [SerializeField]
+    private Sprite PauseImg,ResumeImg;
+    [SerializeField]
+    private GameObject PlayPauseButton;
     // Start is called before the first frame update
     public void RestartGame()
     {
@@ -21,10 +26,12 @@ public class GameplayUIController : MonoBehaviour
         Debug.Log(GamePause);
         if (GamePause)
         {
+	    PlayPauseButton.GetComponent<Image>().sprite=ResumeImg;
             Time.timeScale = 0;
         }
         else
         {
+	    PlayPauseButton.GetComponent<Image>().sprite=PauseImg;
             Time.timeScale = 1;
         }
     }
